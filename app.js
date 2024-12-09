2f29d263ad4d4fe8ef36995f98dde597e822f253
Eminnn034 <emin.seyid97@gmail.com>
2024-12-09 18:52:42 +0400

    apptask

---
+++ app.js
@@ -0,0 +1,45 @@
+// let d = "32,35,37,39,41"
+// let h = d.replaceAll("," ,";");
+// console.log(h);
+
+
+
+
+
+// let  names = ['Hofstadter','Cooper','Wolowtiz','Koothrappali'];
+// arr.sort((a,b) => b-a);
+// console.log(arr);
+
+
+
+
+
+
+// let arr = [10,20,30,40,50];
+// let main = [...arr]
+//    let e = main.map((t)=>{
+
+//     return t*2
+
+//    })
+//    console.log(e);
+
+
+
+//    let arr  =[10,20,30,40,50 ,60,70,80,90,100];
+
+// let [a,b, ... orthers] = arr
+
+// console.log(a);
+// console.log(b);
+// console.log(orthers);
+
+
+// let   arr = [10,15,20,30,40,50,60,70,80,90,10]; 
+//  let a = Math.max(...arr);
+//  console.log(a);
+
+ 
+
+
+
